aae8c35ed91e8e64bff997eb44ea800f25867154
RobinCar <robinc@theodo.fr>
2022-09-14 15:49:51 +0200

    add percy

--- src/components/pokemon/Pokemon.cy.js
+++ src/components/pokemon/Pokemon.cy.js
@@ -1,8 +1,10 @@
 import Pokemon from "./Pokemon";
+import "@percy/cypress";
 
 describe("<Pokemon>", () => {
   it("mounts", () => {
     cy.mount(<Pokemon name={"Bulbasaur"} id={1} weight={7} height={69} />);
     cy.get("[data-cy=name]").should("have.text", "Bulbasaur");
+    cy.percySnapshot();
   });
 });
