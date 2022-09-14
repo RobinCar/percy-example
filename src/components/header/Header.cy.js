7423f5ce98eca389a211892af531a9a11de19a82
RobinCar <robinc@theodo.fr>
2022-09-14 15:49:51 +0200

    add percy

--- src/components/header/Header.cy.js
+++ src/components/header/Header.cy.js
@@ -1,7 +1,9 @@
 import Header from "./Header";
+import "@percy/cypress";
 
 describe("<Header>", () => {
   it("mounts", () => {
     cy.mount(<Header />);
+    cy.percySnapshot();
   });
 });
--- src/components/pokemonList/PokemonList.cy.js
+++ src/components/pokemonList/PokemonList.cy.js
@@ -1,9 +1,11 @@
 import PokemonList from "./PokemonList";
 import data from "../../data.json";
+import "@percy/cypress";
 
 describe("<PokemonList>", () => {
   it("mounts", () => {
     cy.viewport(1024, 768);
     cy.mount(<PokemonList pokemonList={data} />);
+    cy.percySnapshot();
   });
 });
