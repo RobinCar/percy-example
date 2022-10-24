f0e0d07846cb4e50bd7cff714cbf14dfc5202a29
RobinCar <robinc@theodo.fr>
2022-10-24 18:12:48 +0200

    add details route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import data from "./data.json";
 import Header from "./components/header/Header";
 import PokemonList from "./components/pokemonList/PokemonList";
+import PokemonDetails from "./components/about/PokemonDetails";
 import "./App.css";
 
 function App() {
@@ -11,7 +13,13 @@ function App() {
     <div className="app">
       <Header />
       <h1 class="title">Pokedex</h1>
-      <PokemonList pokemonList={pokemonList} />
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<PokemonList pokemonList={pokemonList} />} />
+          <Route path="/details" element={<PokemonDetails />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
